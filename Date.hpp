9a06be3b95e57a6261eb1adae59a06e25c46333d
Ana Anderle <anaclaraanderle2003@gmail.com>
2024-04-30 20:05:21 -0300

    feat: Adding date

---
+++ Date.hpp
@@ -0,0 +1,23 @@
+#ifndef RESTAURANT_MANAGEMENT_DATE_HPP
+#define RESTAURANT_MANAGEMENT_DATE_HPP
+
+
+class Date {
+public:
+    Date(int day, int month, int year);
+    Date();
+
+    int getDay();
+    int getMonth();
+    int getYear();
+    bool setDate(int day, int month, int year);
+    void print();
+    bool validDate(int day, int month, int year);
+private:
+    int day=1;
+    int month=1;
+    int year=2000;
+};
+
+
+#endif
