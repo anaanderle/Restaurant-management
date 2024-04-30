e0c02baad2250b2ce5bc660f35c6044efef0a04e
Ana Anderle <anaclaraanderle2003@gmail.com>
2024-04-30 20:05:42 -0300

    feat: Adding order

---
+++ Order.cpp
@@ -0,0 +1,98 @@
+#include "Item.hpp"
+#include "Order.hpp"
+
+using namespace std;
+
+Order::Order(vector<Item> items, vector<Combo> combos, Date createdAt) {
+    this->orderCodeSequence++;
+    this->code = orderCodeSequence;
+
+    this->items = items;
+    this->combos = combos;
+    this->createdAt = createdAt;
+}
+
+Order::Order(){
+    this->orderCodeSequence++;
+    this->code = orderCodeSequence;
+}
+
+
+int Order::getCode() {
+    return code;
+}
+
+vector<Item> Order::getItems() {
+    return items;
+}
+
+vector<Combo> Order::getCombos() {
+    return combos;
+}
+
+float Order::getTotalValue() {
+    return totalValue;
+}
+
+OrderStatus Order::getOrderStatus() {
+    return orderStatus;
+}
+
+Date Order::getCreatedAt() {
+    return createdAt;
+}
+
+Customer Order::getCustomer() {
+    return customer;
+}
+
+PaymentMethod Order::getPaymentMethod() {
+    return paymentMethod;
+}
+
+void Order::setItems(vector<Item> items) {
+    this->items = items;
+    this->totalValue = this->calculateTotalValue();
+}
+
+void Order::setCombos(vector<Combo> combos) {
+    this->combos = combos;
+    this->totalValue = this->calculateTotalValue();
+}
+
+void Order::setOrderStatus(OrderStatus orderStatus) {
+    this->orderStatus = orderStatus;
+}
+
+void Order::setCustomer(Customer customer) {
+    this->customer = customer;
+}
+
+void Order::addItem(Item item) {
+    this->items.push_back(item);
+    this->totalValue = this->calculateTotalValue();
+}
+
+void Order::addCombo(Combo combo) {
+    this->combos.push_back(combo);
+    this->totalValue = this->calculateTotalValue();
+}
+
+void Order::pay(PaymentMethod paymentMethod) {
+    this->paymentMethod = paymentMethod;
+    this->orderStatus = production;
+}
+
+float Order::calculateTotalValue() {
+    float totalValue = 0;
+
+    for (auto& item : items) {
+        totalValue += item.getPrice();
+    }
+
+    for (auto& combo : combos) {
+        totalValue += combo.getPriceWithDiscount();
+    }
+
+    return totalValue;
+}
---
+++ Order.hpp
@@ -0,0 +1,62 @@
+#ifndef RESTAURANT_MANAGEMENT_ORDER_HPP
+#define RESTAURANT_MANAGEMENT_ORDER_HPP
+
+#include <iostream>
+#include <string>
+#include "Item.hpp"
+#include "Combo.hpp"
+#include "Date.hpp"
+#include "Customer.hpp"
+#include <vector>
+
+using namespace std;
+
+enum OrderStatus {
+    waitingPayment = 'W',
+    production = 'P',
+    delivered = 'D',
+};
+
+enum PaymentMethod {
+    money = 'M',
+    card = 'C',
+    pix = 'P',
+};
+
+class Order {
+public:
+    Order(vector<Item> items, vector<Combo> combos, Date createdAt);
+    Order();
+    
+    int getCode();
+    vector<Item> getItems();
+    vector<Combo> getCombos();
+    float getTotalValue();
+    OrderStatus getOrderStatus();
+    Date getCreatedAt();
+    Customer getCustomer();
+    PaymentMethod getPaymentMethod();
+
+    void setItems(vector<Item> items);
+    void setCombos(vector<Combo> combos);
+    void setOrderStatus(OrderStatus orderStatus);
+    void setCustomer(Customer customer);
+    void addItem(Item item);
+    void addCombo(Combo combo);
+    void pay(PaymentMethod paymentMethod);
+
+private:
+    int code;
+    vector<Item> items;
+    vector<Combo> combos;
+    float totalValue = 0.00;
+    static unsigned int orderCodeSequence;
+    OrderStatus orderStatus = waitingPayment;
+    PaymentMethod paymentMethod;
+    Date createdAt;
+    Customer customer;
+
+    float calculateTotalValue();
+};
+
+#endif
